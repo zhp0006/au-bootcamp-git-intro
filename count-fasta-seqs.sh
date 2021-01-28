c71079bae0f2ca4ec58ed9e774c3ce7050108e24
Lacona <azp0136@auburn.edu>
2021-01-27 21:22:48 -0600

    Adding filename and printing the output in one line

--- count-fasta-seqs.sh
+++ count-fasta-seqs.sh
@@ -100,3 +100,4 @@ echo "$@"
 
 grep -o -i "Negros" example-seqs1.fasta | wc -l; grep -o -i "Negros" example-seqs2.fasta | wc -l; grep -o -i "Negros" example-seqs1.fasta example-seqs2.fasta | wc -l
 
+for f in *.fasta; do grep -o -i "Negros" ${f} | wc -l | tr -d '\n'; echo ${f}; done;
