3c2c4d319d31737bfef943ea97c19f56e62d95f1
Lacona <azp0136@auburn.edu>
2021-01-31 07:49:38 -0600

    Solved the total count, test output looks perfect, but test is not happy and says "failed!!!"

--- count-fasta-seqs.sh
+++ count-fasta-seqs.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 # works for each file alone but the sum of both is still missing 
-for f in "$@"; do grep -o -i "Negros" ${f} | wc -l | tr -d '\n'; basename ${f}; done
-grep -o -i "Negros" ${f} | wc -l
-
-
+for f in "$@"; do grep -o -i ">" ${f} | wc -l | tr -d '\n'; basename ${f}; done
+cat "$@" | grep -c ">" 
