347e850dd1fd8bd98691f415209cb2f0fc1e9f7d
Lacona <azp0136@auburn.edu>
2021-01-31 04:51:25 -0600

    Divided the code for example1 and example2 and added total count

--- count-fasta-seqs.sh
+++ count-fasta-seqs.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-# Counting the  sequences in the individual fasta file using regular expressions
-grep -o -i "Negros" example-seqs1.fasta | wc -l; grep -o -i "Negros" example-seqs2.fasta | wc -l; grep -o -i "Negros" example-seqs1.fasta example-seqs2.fasta | wc -l
- 
-# Counting total number of sequences  
-for f in *.fasta; do grep -o -i "Negros" ${f} | wc -l | tr -d '\n'; echo ${f}; done;
+# Number of sequences in example1 based on "Negros"
+for f in example-seqs1.fasta; do grep -o -i "Negros" ${f} | wc -l | tr -d '\n'; echo ${f}; done
+
+# Number of sequences in example2 based on "Negros"
+for g in example-seqs2.fasta; do grep -o -i "Negros" ${g} | wc -l | tr -d '\n';
+echo ${g}; done
+
+# Total number of sequences in example1 and example2 based on "Negros"
+grep -o -i "Negros" ${f} ${g} | wc -l
