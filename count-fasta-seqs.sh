c5b89697529189da8a4c035d5f653a40c8c46eec
Lacona <azp0136@auburn.edu>
2021-01-31 06:15:00 -0600

    Works for all combinations of files, but total count for both file is not working and also there should be a space between the count and the filename

--- count-fasta-seqs.sh
+++ count-fasta-seqs.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-# Number of sequences in example1 based on "Negros"
-for f in example-seqs1.fasta; do grep -o -i "Negros" ${f} | wc -l | tr -d '\n'; echo ${f}; done
+# works for each file alone but the sum of both is still missing 
+for f in "$@"; do grep -o -i "Negros" ${f} | wc -l | tr -d '\n'; basename ${f}; done
+grep -o -i "Negros" ${f} | wc -l
 
-# Number of sequences in example2 based on "Negros"
-for g in example-seqs2.fasta; do grep -o -i "Negros" ${g} | wc -l | tr -d '\n';
-echo ${g}; done
 
-# Total number of sequences in example1 and example2 based on "Negros"
-grep -o -i "Negros" ${f} ${g} | wc -l
