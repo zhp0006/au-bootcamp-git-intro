56b63ad609139e72529bc37df41e3c47d073604b
Lacona <azp0136@auburn.edu>
2021-02-02 15:55:16 -0600

    Code is done and passes the test

--- count-fasta-seqs.sh
+++ count-fasta-seqs.sh
@@ -3,5 +3,5 @@
 # works perfectly fine, test output looks like it should, but fails nonetheless
 # left align is not possible, who knows why...
 
-for f in "$@"; do count=$(grep -o -i ">" ${f} | wc -l); name=$(basename ${f}); printf '%-10s\n' "$count $name"; done
+for f in "$@"; do count=$(grep -o -i ">" ${f} | wc -l | xargs); name=$(basename ${f}); echo "$count $name"; done
 cat "$@" | grep -c ">"
