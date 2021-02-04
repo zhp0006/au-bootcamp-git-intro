d1b91655f8e89e0e7673316917ed752cbe293619
Wang <bzw0059@auburn.edu>
2021-02-04 11:26:24 -0600

    addding

--- count-fasta-seqs.sh
+++ count-fasta-seqs.sh
@@ -2,7 +2,7 @@
 
 # works perfectly fine, test output looks like it should, but fails nonetheless
 # left align is not possible, who knows why...
-
+hello world
 for f in "$@"; do count=$(grep -o -i ">" ${f} | wc -l | xargs); name=$(basename ${f}); echo "$count $name"; done
 cat "$@" | grep -c ">"
 hsfk;lusiodcv
